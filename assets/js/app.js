ffae06971cbeef16339f0409462f09cfd7bf07fb
Logan Sorese <harvey@Harveys-MacBook-Pro.local>
2016-12-19 12:29:11 -0800

    Commit Sub

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,24 @@
 $(function() {
     $('.header__menu__desktop ul').slicknav({});
-    jQuery('.toggle--mobile').on('click', function() {
+    jQuery('.toggle--mobile').on('click', function(e) {
+        e.preventDefault();
         $('.header__menu__desktop ul').slicknav('toggle');
     });
-	$('.options').parallax({imageSrc: 'assets/images/parallax--building--o.png'});
-	$('.clients').parallax({imageSrc: 'assets/images/parallax--building--o.png'});
-$('.clients__slideshow').owlCarousel({
-    loop:true,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+    $('.options').parallax({ imageSrc: 'assets/images/parallax--building--o.png' });
+    $('.clients').parallax({ imageSrc: 'assets/images/parallax--building--o.png' });
+    $('.clients__slideshow').owlCarousel({
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
         }
-    }
-})
+    })
 });
